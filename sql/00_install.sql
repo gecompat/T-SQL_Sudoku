ca5c1a14af38058ae03b7c04a8e0086f9e92b85e
Gerhard P <github@gecomp.at>
2026-07-18 00:15:18 +0200

    Run diagnostic definition hardening

--- sql/00_install.sql
+++ sql/00_install.sql
@@ -6,6 +6,7 @@
 :r $(RootPath)/install/10_USP_SudokuValidate.sql
 :r $(RootPath)/install/20_USP_SudokuSolve.sql
 :r $(RootPath)/install/25_USP_SudokuDiagnoseFirstDeduction.sql
+:r $(RootPath)/install/27_diagnostic_definition_hardening.sql
 :r $(RootPath)/install/30_temp_constraint_hardening.sql
 :r $(RootPath)/install/35_status_contract_hardening.sql
 
