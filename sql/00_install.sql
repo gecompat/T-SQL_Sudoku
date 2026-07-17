038c94715986c201192f435019cae59fabb0d000
Gerhard P <github@gecomp.at>
2026-07-17 23:35:29 +0200

    Use portable SQLCMD include paths

--- sql/00_install.sql
+++ sql/00_install.sql
@@ -1,9 +1,9 @@
 :setvar RootPath "."
 
-:r $(RootPath)\install\00_tables.sql
-:r $(RootPath)\install\05_technique_mode_corrections.sql
-:r $(RootPath)\install\10_USP_SudokuValidate.sql
-:r $(RootPath)\install\20_USP_SudokuSolve.sql
+:r $(RootPath)/install/00_tables.sql
+:r $(RootPath)/install/05_technique_mode_corrections.sql
+:r $(RootPath)/install/10_USP_SudokuValidate.sql
+:r $(RootPath)/install/20_USP_SudokuSolve.sql
 
 PRINT 'T-SQL Sudoku installation completed.';
 GO
