320cc402b9f631857ead92bee7117bfdd4d1d8e7
Gerhard P <github@gecomp.at>
2026-07-18 00:25:00 +0200

    Add shared state reset hardening to installation

--- sql/00_install.sql
+++ sql/00_install.sql
@@ -8,6 +8,7 @@
 :r $(RootPath)/install/25_USP_SudokuDiagnoseFirstDeduction.sql
 :r $(RootPath)/install/26_shared_deduction_engine.sql
 :r $(RootPath)/install/27_diagnostic_definition_hardening.sql
+:r $(RootPath)/install/28_shared_state_reset_hardening.sql
 :r $(RootPath)/install/30_temp_constraint_hardening.sql
 :r $(RootPath)/install/35_status_contract_hardening.sql
 
