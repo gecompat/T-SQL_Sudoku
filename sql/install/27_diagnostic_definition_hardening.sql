e55291b2b13bb018e55b124d7d9d2f9894102b56
Gerhard P <github@gecomp.at>
2026-07-18 00:15:07 +0200

    Harden diagnostic procedure definition

---
+++ sql/install/27_diagnostic_definition_hardening.sql
@@ -0,0 +1,33 @@
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+GO
+
+DECLARE @Definition nvarchar(max);
+
+SELECT
+    @Definition = ModuleDefinition.[definition]
+FROM sys.sql_modules AS ModuleDefinition
+WHERE ModuleDefinition.[object_id] =
+      OBJECT_ID(N'dbo.USP_SudokuDiagnoseFirstDeduction', N'P');
+
+IF @Definition IS NULL
+BEGIN
+    THROW 50510,
+          'Diagnostic procedure was not found for definition hardening.',
+          1;
+END;
+
+SET @Definition = REPLACE
+(
+    @Definition,
+    N'                AND
+                (
+                    (@UnitType IS NULL AND 1 = 1)
+                    OR 1 = 1
+                )
+                AND',
+    N'                AND'
+);
+
+EXEC sys.sp_executesql @Definition;
+GO
