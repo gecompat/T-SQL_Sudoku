32030908a8b262a0ac2a78f73639a6059fb9a224
Gerhard P <github@gecomp.at>
2026-07-18 00:13:11 +0200

    Install diagnostic deduction components

--- sql/00_install.sql
+++ sql/00_install.sql
@@ -2,8 +2,10 @@
 
 :r $(RootPath)/install/00_tables.sql
 :r $(RootPath)/install/05_technique_mode_corrections.sql
+:r $(RootPath)/install/07_diagnostic_types.sql
 :r $(RootPath)/install/10_USP_SudokuValidate.sql
 :r $(RootPath)/install/20_USP_SudokuSolve.sql
+:r $(RootPath)/install/25_USP_SudokuDiagnoseFirstDeduction.sql
 :r $(RootPath)/install/30_temp_constraint_hardening.sql
 :r $(RootPath)/install/35_status_contract_hardening.sql
 
