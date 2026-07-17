9f61af0ba4e0674f4801e1cc45145f8eb72189ce
Gerhard P <github@gecomp.at>
2026-07-17 22:31:03 +0200

    Include technique mode corrections

--- sql/00_install.sql
+++ sql/00_install.sql
@@ -1,6 +1,7 @@
 :setvar RootPath "."
 
 :r $(RootPath)\install\00_tables.sql
+:r $(RootPath)\install\05_technique_mode_corrections.sql
 :r $(RootPath)\install\10_USP_SudokuValidate.sql
 :r $(RootPath)\install\20_USP_SudokuSolve.sql
 
