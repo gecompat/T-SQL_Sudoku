a443ca29e073dd46fccf01c4d802590761e52562
Gerhard P <github@gecomp.at>
2026-07-18 02:01:53 +0200

    Make shared state hardening idempotent

--- sql/install/28_shared_state_reset_hardening.sql
+++ sql/install/28_shared_state_reset_hardening.sql
@@ -2,7 +2,9 @@ SET NOCOUNT ON;
 SET XACT_ABORT ON;
 GO
 
-DECLARE @Definition nvarchar(max);
+DECLARE
+    @Definition nvarchar(max),
+    @ParameterStart int;
 
 SELECT
     @Definition = ModuleDefinition.[definition]
@@ -16,15 +18,15 @@ BEGIN
           1;
 END;
 
-IF @Definition NOT LIKE N'%DECLARE @SharedCandidateState dbo.SudokuCandidateState;%'
-   OR @Definition NOT LIKE N'%DECLARE @SharedDeduction TABLE%'
+IF CHARINDEX(N'DECLARE @SharedCandidateState dbo.SudokuCandidateState;', @Definition) = 0
+   OR CHARINDEX(N'DECLARE @SharedDeduction TABLE', @Definition) = 0
 BEGIN
     THROW 50531,
           'The shared deduction state declarations were not found in the solver.',
           1;
 END;
 
-IF @Definition NOT LIKE N'%DELETE FROM @SharedCandidateState;%'
+IF CHARINDEX(N'DELETE FROM @SharedCandidateState;', @Definition) = 0
 BEGIN
     SET @Definition = REPLACE
     (
@@ -37,13 +39,27 @@ BEGIN
     );
 END;
 
-IF @Definition NOT LIKE N'%DELETE FROM @SharedCandidateState;%'
-   OR @Definition NOT LIKE N'%DELETE FROM @SharedDeduction;%'
+IF CHARINDEX(N'DELETE FROM @SharedCandidateState;', @Definition) = 0
+   OR CHARINDEX(N'DELETE FROM @SharedDeduction;', @Definition) = 0
 BEGIN
     THROW 50532,
           'The shared deduction state reset could not be applied.',
           1;
 END;
+
+SET @ParameterStart = CHARINDEX(N'(', @Definition);
+
+IF @ParameterStart = 0
+BEGIN
+    THROW 50533,
+          'The solver parameter-list marker was not found during shared-state hardening.',
+          1;
+END;
+
+SET @Definition =
+    N'ALTER PROCEDURE dbo.USP_SudokuSolve' +
+    CHAR(13) + CHAR(10) +
+    SUBSTRING(@Definition, @ParameterStart, LEN(@Definition));
 
 EXEC sys.sp_executesql @Definition;
 GO
