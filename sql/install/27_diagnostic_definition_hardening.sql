ce95a8188e7ff189b96ced5872e4088486d09f1c
Gerhard P <github@gecomp.at>
2026-07-18 02:01:41 +0200

    Make diagnostic hardening idempotent

--- sql/install/27_diagnostic_definition_hardening.sql
+++ sql/install/27_diagnostic_definition_hardening.sql
@@ -2,7 +2,9 @@ SET NOCOUNT ON;
 SET XACT_ABORT ON;
 GO
 
-DECLARE @Definition nvarchar(max);
+DECLARE
+    @Definition nvarchar(max),
+    @ParameterStart int;
 
 SELECT
     @Definition = ModuleDefinition.[definition]
@@ -28,6 +30,20 @@ SET @Definition = REPLACE
                 AND',
     N'                AND'
 );
+
+SET @ParameterStart = CHARINDEX(N'(', @Definition);
+
+IF @ParameterStart = 0
+BEGIN
+    THROW 50511,
+          'Shared deduction engine parameter-list marker was not found during hardening.',
+          1;
+END;
+
+SET @Definition =
+    N'ALTER PROCEDURE dbo.USP_SudokuFindFirstDeduction' +
+    CHAR(13) + CHAR(10) +
+    SUBSTRING(@Definition, @ParameterStart, LEN(@Definition));
 
 EXEC sys.sp_executesql @Definition;
 GO
